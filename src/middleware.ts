4a8aead04dfef3930b0d01a01853304275645049
junya-thinkactive-d <develop.junya@gmail.com>
2023-08-13 09:35:12 +0900

    test

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,14 @@
-import type { NextRequest } from 'next/server';
-import { MiddlewareRequest } from '@netlify/next';
+import { NextResponse, type NextRequest } from 'next/server';
 
-import type { NextRequest as NetlifyNextRequest } from '@netlify/next';
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next();
 
-export async function middleware(nextRequest: NextRequest) {
-  const pathname = nextRequest.nextUrl.pathname;
-
-  const middlewareRequest = new MiddlewareRequest(
-    nextRequest as NetlifyNextRequest
-  );
-
-  if (pathname.startsWith('/static')) {
-    const response = await middlewareRequest.next();
-
-    const message = `This was a static page but has been transformed in ${nextRequest?.geo?.city},${nextRequest?.geo?.country} using @netlify/next middleware.ts`;
-
-    response.replaceText('#message', message);
-
-    return response;
+  if (req.nextUrl.pathname !== '/sign-in') {
+    return NextResponse.redirect(new URL('/sign-in', req.url));
   }
+  return res;
 }
+
+export const config = {
+  matcher: ['/', '/sign-in'],
+};
